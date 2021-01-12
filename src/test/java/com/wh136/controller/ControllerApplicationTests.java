40e43d4bfcc78e3ddaf5b241724b3ace8a37e27a
xiaweihua <136856026@qq.com>
2021-01-12 18:47:32 +0800

    first commit

---
+++ src/test/java/com/wh136/controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wh136.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
